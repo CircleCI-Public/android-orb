eadb1b6101cbb71f1fcb0d0ff9ceba4f8a44bec9
ryanbourdais <rbourd4@lsu.edu>
2022-06-28 10:54:04 -0500

    fix: added eval and reworded default value

--- src/scripts/generate-cache-checksum.sh
+++ src/scripts/generate-cache-checksum.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-find ${PARAM_FIND_ARGS} | sort | xargs cat |
-shasum | awk '{print $1}' > /tmp/gradle_cache_seed
+echo "The following are the files used to generate the cache checksum:"
+eval find "${PARAM_FIND_ARGS}"
+eval find "${PARAM_FIND_ARGS}" | sort | xargs cat |
+shasum | awk '{print $1}' > /tmp/gradle_cache_seed 
