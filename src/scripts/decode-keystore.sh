c5d9ee3ac80ab580702549da49275545db546cce
ryanbourdais <rbourd4@lsu.edu>
2022-07-21 16:44:07 -0500

    fix: capitalization and immutable vars

--- src/scripts/decode-keystore.sh
+++ src/scripts/decode-keystore.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo ${PARAM_BASE64_KEYSTORE} | base64 -d | tee keystore android/app/keystore > /dev/null
+
+PARAM_BASE64_KEYSTORE=$(eval echo "\$$PARAM_BASE64_KEYSTORE")
+
+echo "$PARAM_BASE64_KEYSTORE" | base64 -di | tee keystore "${PARAM_KEYSTORE_LOCATION}" > /dev/null
