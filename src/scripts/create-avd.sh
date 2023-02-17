207a5bea6d4832176c0514bc70953a0a0b0cb8cb
ryanbourdais <ryanbourdais@outlook.com>
2023-02-17 10:53:33 -0600

    TEST

--- src/scripts/create-avd.sh
+++ src/scripts/create-avd.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-SYSTEM_IMAGE="${!PARAM_SYSTEM_IMAGE}"
+
+IMAGE="${!PARAM_SYSTEM_IMAGE}"
 
 if [ "${PARAM_INSTALL}" == 1 ]; then
-    sdkmanager "$SYSTEM_IMAGE"
+    sdkmanager "$IMAGE"
 fi
 
-echo "no" | avdmanager --verbose create avd -n ${PARAM_AVD_NAME} -k "$SYSTEM_IMAGE" ${PARAM_ADDITIONAL_ARGS}
+echo "no" | avdmanager --verbose create avd -n ${PARAM_AVD_NAME} -k "$IMAGE" ${PARAM_ADDITIONAL_ARGS}
