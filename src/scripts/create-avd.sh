25c695a36df3b48de2962783abaec196cbfe6bfb
ryanbourdais <ryanbourdais@outlook.com>
2023-02-17 11:35:28 -0600

    test

--- src/scripts/create-avd.sh
+++ src/scripts/create-avd.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-SYSTEM_IMAGE='${!PARAM_SYSTEM_IMAGE}'
-
-echo $SYSTEM_IMAGE
-echo $PARAM_SYSTEM_IMAGE
-echo (eval $PARAM_SYSTEM_IMAGE)
-
 if [ "${PARAM_INSTALL}" == 1 ]; then
     sdkmanager "$PARAM_SYSTEM_IMAGE"
 
