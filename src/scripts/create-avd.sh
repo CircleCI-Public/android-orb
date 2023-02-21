b9c7773f4048d6710f2977c058c650ee2bc45f81
Ryan Bourdais <67397011+ryanbourdais@users.noreply.github.com>
2023-02-21 11:10:45 -0600

    feat: added background to create-avd (#65)
    * feat: added background param to create-avd

--- src/scripts/create-avd.sh
+++ src/scripts/create-avd.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+if [ "${PARAM_INSTALL}" == 1 ]; then
+    sdkmanager "${PARAM_SYSTEM_IMAGE}"
+fi
+
 echo "no" | avdmanager --verbose create avd -n ${PARAM_AVD_NAME} -k ${PARAM_SYSTEM_IMAGE} ${PARAM_ADDITIONAL_ARGS}
