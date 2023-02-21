a6234cc8a8327e84002006272fc07ebf839da9e3
ryanbourdais <ryanbourdais@outlook.com>
2023-02-21 12:34:01 -0600

    test

--- src/scripts/create-avd.sh
+++ src/scripts/create-avd.sh
@@ -7,4 +7,4 @@ if [ "${PARAM_INSTALL}" == 1 ]; then
 
 fi
 
-echo "no" | avdmanager --verbose create avd -n ${PARAM_AVD_NAME} -k $IMAGE ${PARAM_ADDITIONAL_ARGS}
+echo "no" | avdmanager --verbose create avd -n ${PARAM_AVD_NAME} -k "$IMAGE" ${PARAM_ADDITIONAL_ARGS}
