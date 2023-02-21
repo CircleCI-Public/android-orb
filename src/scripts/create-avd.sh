d5eb0c7bada4c74828291a3fb080f76a651c2498
ryanbourdais <ryanbourdais@outlook.com>
2023-02-21 14:49:19 -0600

    test

--- src/scripts/create-avd.sh
+++ src/scripts/create-avd.sh
@@ -5,6 +5,9 @@ IMAGE=$(eval echo ${ORB_EVAL_PARAM_SYSTEM_IMAGE})
 if [ "${PARAM_INSTALL}" == 1 ]; then
     sdkmanager ${IMAGE}
     echo ${IMAGE}
+    echo ${IMAGE}
+    echo ${IMAGE}
+    echo ${IMAGE}
     echo "installed"
 
 fi
