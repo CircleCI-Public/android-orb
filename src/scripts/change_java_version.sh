3b16935d1f86fb3cccf7fa85779ee6693ac3148a
Mateo Arboleda <mateo.arboleda@circleci.com>
2024-10-02 09:56:57 -0500

    Extra fixes

--- src/scripts/change_java_version.sh
+++ src/scripts/change_java_version.sh
@@ -17,8 +17,8 @@ if [ "$CURRENT_JAVA_VER" -ne "${PARAM_JAVA_VER}" ]; then
     sudo update-alternatives --set javac /usr/lib/jvm/java-"${PARAM_JAVA_VER}"-openjdk-amd64/bin/javac
     sudo update-alternatives --set java /usr/lib/jvm/java-"${PARAM_JAVA_VER}"-openjdk-amd64/bin/java
   fi
-  echo "export JAVA_HOME=/usr/lib/jvm/java-${PARAM_JAVA_VER}-openjdk-amd64" >>~/.bashrc
-  echo "export PATH=$JAVA_HOME/bin:$PATH" >>~/.bashrc
+  echo "export JAVA_HOME=/usr/lib/jvm/java-${PARAM_JAVA_VER}-openjdk-amd64" >> "$BASH_ENV"
+  echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> "$BASH_ENV"
 fi
 NEW_JAVA_VER="$(java -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)"
 NEW_JAVAC_VER="$(javac -version 2>&1 | head -1 | cut -f 2- -d ' ' | sed '/^1\./s///' | cut -d'.' -f1)"
