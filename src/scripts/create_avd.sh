6812815995e323729a2d9bac3a0c0143a7508656
marboledacci <mateo.arboleda@circleci.com>
2024-10-21 09:44:35 -0500

    Fix issue when replacing arguments with circleci env subst (#105)
    * Extra test with args
    
    * Add debug to new test
    
    * Fix shell
    
    * Add shell to executor
    
    * update the way the subst is done on scripts
    
    * Do the substitution in a more explicit way
    
    * remove debug

--- src/scripts/create_avd.sh
+++ src/scripts/create_avd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-PARAM_AVD_NAME="$(circleci env subst "$PARAM_AVD_NAME")"
-PARAM_SYSTEM_IMAGE="$(circleci env subst "$PARAM_SYSTEM_IMAGE")"
-PARAM_ADDITIONAL_ARGS="$(circleci env subst "$PARAM_ADDITIONAL_ARGS")"
+PARAM_AVD_NAME="$(echo "$PARAM_AVD_NAME" | circleci env subst "$PARAM_AVD_NAME")"
+PARAM_SYSTEM_IMAGE="$(echo "$PARAM_SYSTEM_IMAGE" | circleci env subst "$PARAM_SYSTEM_IMAGE")"
+PARAM_ADDITIONAL_ARGS="$(circleci env subst -- "$PARAM_ADDITIONAL_ARGS")"
 
 if [ "${PARAM_INSTALL}" == 1 ]; then
     sdkmanager "${PARAM_SYSTEM_IMAGE}"
